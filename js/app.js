34832af63535a30c1857005304f2f81ae6102d0f
Pin Chen <pamyc1876@gmail.com>
2022-12-17 21:45:40 -0500

    Make snake not moving reverse

--- js/app.js
+++ js/app.js
@@ -30,21 +30,20 @@ const snake = {
     lastTailPosition: 0,
 }
 
-const moveDirections = [[0,0],[0,1],[0,-1],[1,0],[1,1],[1,-1],[-1,0],[-1,1],[-1,-1]];
+const moveDirections = [[0,1],[0,-1],[1,0],[1,1],[1,-1],[-1,0],[-1,1],[-1,-1]];
 /*---------------------------- Variables (state) ----------------------------*/
-let start = false, pause = false, win = false, lost = false, moveIndex = 1, sqrIdx, oldHeadSqrIdx;
+let start = false, pause = true, win = false, lost = false, newMoveIdx, moveIdx = 0, sqrIdx, oldHeadSqrIdx;
 let headPosition1 = 11, headPosition2 = 11, lastTailPosition1, lastTailPosition2, lastTailIdx;
 let movePosition1, movePosition2, eatFruit = false;
 
-//div*484.sqr#spr0000>img
 /*------------------------ Cached Element References ------------------------*/
 const boardEl = document.querySelector(".board");
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 boardEl.addEventListener('click', changeDirection);
-document.querySelector('#start-bt').addEventListener('click', game);
-document.querySelector('#pause-bt').addEventListener('click', () => pause = !pause);
+document.querySelector('#start-bt').addEventListener('click', () => pause = false);
+document.querySelector('#pause-bt').addEventListener('click', () => pause = true);
 document.querySelector('#eat-bt').addEventListener('click', () => eatFruit = true);
 
 
@@ -52,55 +51,56 @@ document.querySelector('#eat-bt').addEventListener('click', () => eatFruit = tru
 function changeDirection(e) {
     if(!pause){
         let directionId = +e.target.id.replace('sqr', '');
-        console.log("🚀 ~ file: app.js:49 ~ changeDirection ~ directionId", directionId)
         movePosition1 = Math.floor(directionId/board.length);
-        console.log("🚀 ~ file: app.js:50 ~ changeDirection ~ movePosition1", movePosition1)
         movePosition2 = directionId%board.length;
-        console.log("🚀 ~ file: app.js:52 ~ changeDirection ~ movePosition2", movePosition2)
         if(movePosition1 === headPosition1){
             if(movePosition2 === headPosition2) ;//moving direction not change
-            else if(movePosition2 > headPosition2) moveIndex = 1;
-            else if(movePosition2 < headPosition2) moveIndex = 2;
+            else if(movePosition2 > headPosition2) newMoveIdx = 0;
+            else if(movePosition2 < headPosition2) newMoveIdx = 1;
         }
         else if(movePosition1 > headPosition1){
-            if(movePosition2 === headPosition2) moveIndex = 3;
-            else if(movePosition2 > headPosition2) moveIndex = 4;
-            else if(movePosition2 < headPosition2) moveIndex = 5;
+            if(movePosition2 === headPosition2) newMoveIdx = 2;
+            else if(movePosition2 > headPosition2) newMoveIdx = 3;
+            else if(movePosition2 < headPosition2) newMoveIdx = 4;
         }
         else if(movePosition1 < headPosition1){
-            if(movePosition2 === headPosition2) moveIndex = 6;
-            else if(movePosition2 > headPosition2) moveIndex = 7;
-            else if(movePosition2 < headPosition2) moveIndex = 8;
+            if(movePosition2 === headPosition2) newMoveIdx = 5;
+            else if(movePosition2 > headPosition2) newMoveIdx = 6;
+            else if(movePosition2 < headPosition2) newMoveIdx = 7;
+        }
+        console.log("old", moveIdx, moveIdx[0],moveIdx[1], "new", newMoveIdx,newMoveIdx[0],newMoveIdx[1])
+        //snake should not be able to go reverse direction
+        if(!( moveDirections[moveIdx][0] === -moveDirections[newMoveIdx][0] &&
+             moveDirections[moveIdx][1] === -moveDirections[newMoveIdx][1])){
+            moveIdx = newMoveIdx;
         }
     }
-    // gamePlay();
 }
 
-
 function game() {
+    // pause = false;
     let running = setInterval(gamePlay, 400);
     if(win || lost)
         clearInterval(running);
 }
 
 function gamePlay() {
-    if(pause) moveIndex = 0;
+    if(!pause){
     snakeMove();
     render();
+    }
 }
 
-function snakeMove() {
-    // lastTailPosition1 = 
-    // lastTailPosition2 = 
+function snakeMove() { 
     oldHeadSqrIdx = 22*(headPosition1) + headPosition2;
     if(eatFruit)
         snake.tailLength++;
 
     if(snake.tailLength){
-        snake.tailNo =`#a${headPosition1}b${headPosition2}` + snake.tailNo;
-        console.log("🚀 ~ file: app.js:98 ~ snakeMove ~ tailNo", snake.tailNo)
-        console.log("tailLegnth", snake.tailLength)
-        let tailStr = snake.tailNo;
+        snake.tailNo =`a${headPosition1}b${headPosition2}` + snake.tailNo;
+
+        let tailStr = snake.tailNo; //make a copy.
+        //get the positions from the string.
         for(let i = tailStr.length -1; i >= 0; i--){
             if(tailStr[i] === 'b') {
                 lastTailPosition2 = +(tailStr.slice(i).replace('b',''));
@@ -108,8 +108,8 @@ function snakeMove() {
                 if(!eatFruit)
                     snake.tailNo = snake.tailNo.slice(0, i);
             }
-            else if (tailStr[i] === '#'){
-                lastTailPosition1 = +(tailStr.slice(i).replace("#a",''));
+            else if (tailStr[i] === 'a'){
+                lastTailPosition1 = +(tailStr.slice(i).replace("a",''));
                 //we don't need to update the tailStr(since we done with it) here.
                 if(!eatFruit)
                     snake.tailNo = snake.tailNo.slice(0, i);
@@ -123,8 +123,8 @@ function snakeMove() {
         lastTailIdx = oldHeadSqrIdx;
     }
     
-    headPosition1 += moveDirections[moveIndex][0];
-    headPosition2 += moveDirections[moveIndex][1];
+    headPosition1 += moveDirections[moveIdx][0];
+    headPosition2 += moveDirections[moveIdx][1];
     sqrIdx = 22*(headPosition1) + headPosition2;
 }
 
